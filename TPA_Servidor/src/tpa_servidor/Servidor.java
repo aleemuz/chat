cf926175a4ebe3c9fe68986b520eccbad80ebbbf
Alejandra <denuevo_samm@hotmail.com>
2015-02-24 10:14:16 -0700

    Servidor Cliente Chat

---
+++ TPA_Servidor/src/tpa_servidor/Servidor.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tpa_servidor;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Alejandra Muñiz
+ */
+public class Servidor implements AvisaServidor{
+    
+    private ServerSocket serverSocket;    
+    
+    Vector<ClienteThread> clientes;
+    
+    public Servidor(){
+        
+    }
+    
+    void conexion(int puerto){
+        try {
+            
+            clientes = new Vector<ClienteThread>();
+                        
+            serverSocket = new ServerSocket(puerto);
+            System.out.println("Servidor escuchando en el puerto: "+puerto);
+            while(true){
+                Socket socket = serverSocket.accept();
+                System.out.println("Un usuario se conecto");
+                
+                ClienteThread cliente = new ClienteThread(socket);
+                cliente.addListener(this);
+                clientes.add(cliente);
+            }
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }    
+    
+    public void cerrarConexion(){
+        try{
+            serverSocket.close();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClientReceive(String msg) {
+        System.out.println("Llego mensaje al cliente." + msg);
+    }
+    
+}
---
+++ TPA_Servidor/src/tpa_servidor/TPA_Servidor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tpa_servidor;
+
+/**
+ *
+ * @author Alejandra Muñiz
+ */
+public class TPA_Servidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Servidor servidor = new Servidor();
+        servidor.conexion(5555);
+    }
+    
+}
